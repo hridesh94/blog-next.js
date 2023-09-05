10fa159dabc093e6596a5c3dc71d85ff75872c61
Hridesh Sapkota <csit2077.94@pmc.edu.np>
2023-09-06 00:29:40 +0545

    meta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Hridesh Sapkota Blog',
+  author: 'Hridesh Sapkota',
+  headerTitle: 'HrideshBlog',
+  description: 'A personal blog by Hridesh Sapkota',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog-next-js-mocha.vercel.app/',
+  siteRepo: 'https://github.com/hridesh94/blog-next.js.git',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
